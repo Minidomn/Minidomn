fddb84e0226474ad5ec63d744426ac43a61bc763
Minidomn <105628357+Minidomn@users.noreply.github.com>
2022-09-03 14:56:23 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,4 +49,12 @@ def weth_amout(user, weth):
     user.transfer(weth, weth_amout)
     yield weth_amout
 
+@pytest.fixture
+def vault(pm, gov, rewards, guardian, management, token):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault)
+    vault.initialize(token, gov, rewards, "", "", guardian, management)
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.setManagement(management, {"from": gov})
+    yield vault
 
