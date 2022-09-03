6d9d83cce297538448b806430dc5084f4e0d14f7
Minidomn <105628357+Minidomn@users.noreply.github.com>
2022-09-03 14:58:29 +0800

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -26,6 +26,48 @@ def test_operation(
         pytest.approx(token.balanceOf(user), rel=RELATIVE_APPROX) == user_balance_before
     )
 
+    def test_emergency_exit(
+    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
+):
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": user})
+    vault.deposit(amount, {"from": user})
+    chain.sleep(1)
+    strategy.harvest()
+    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+
+    # set emergency and exit
+    strategy.setEmergencyExit()
+    chain.sleep(1)
+    strategy.harvest()
+    assert strategy.estimatedTotalAssets() < amount
+
+
+def test_profitable_harvest(
+    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
+):
+    # Deposit to the vault
+    token.approve(vault.address, amount, {"from": user})
+    vault.deposit(amount, {"from": user})
+    assert token.balanceOf(vault.address) == amount
+
+    # Harvest 1: Send funds through the strategy
+    chain.sleep(1)
+    strategy.harvest()
+    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+
+    # TODO: Add some code before harvest #2 to simulate earning yield
+
+    # Harvest 2: Realize profit
+    chain.sleep(1)
+    strategy.harvest()
+    chain.sleep(3600 * 6)  # 6 hrs needed for profits to unlock
+    chain.mine(1)
+    profit = token.balanceOf(vault.address)  # Profits go to vault
+    # TODO: Uncomment the lines below
+    # assert token.balanceOf(strategy) + profit > amount
+    # assert vault.pricePerShare() > before_pps
+
 def test_triggers(
     chain, gov, vault, strategy, token, amount, user, weth, weth_amout, strategist
 ):
